c94afad671d83dd4989724df8969af832f828057
Thomas <tg.59290@gmail.com>
2020-05-13 17:02:20 +0200

    create function to get player name and call it in game

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ var scores = {
   "player" : 0,
   "computer" : 0
 };
+
+// Functions
+function askPlayerName() {
+  var name = prompt("Veuillez indiquer votre nom avant de jouer");
+  while (name.length < 2 || name.length > 20) {
+    var name = prompt("Votre nom doit être compris entre 2 et 20 caractères");
+  }
+  return name;
+}
+
+// Code logic
+alert("Bonjour et bienvenue. Vous allez jouer au jeu du chifoumi sur votre navigateur grâce à JavaScript");
+var playerName = askPlayerName();
+alert("Parfait " + playerName + " prêt à jouer ?");
