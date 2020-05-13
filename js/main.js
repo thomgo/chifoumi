bcb12bf960a95d5e845f08dece22da630a70c3c6
Thomas <tg.59290@gmail.com>
2020-05-13 17:00:58 +0200

    add global variables for choices, scores and winningPairs

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Global variables
+var choices = ["pierre", "feuille", "ciseaux"];
+
+var winningPairs = [
+  "feuille/pierre",
+  "pierre/ciseaux",
+  "ciseaux/feuille"
+];
+
+var scores = {
+  "player" : 0,
+  "computer" : 0
+};
